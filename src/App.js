2cb201d6ec67deddc4c9b2954db57cbcaa9743bb
lythanhhai <lythanhhait@gmail.com>
2022-11-25 17:14:07 +0700

    setup sentry for monitoring exception

--- src/App.js
+++ src/App.js
@@ -52,103 +52,134 @@ import { useMaterialUIController, setMiniSidenav, setOpenConfigurator } from "co
 // Images
 import brandWhite from "assets/images/logo-ct.png";
 import brandDark from "assets/images/logo-ct-dark.png";
+import * as Sentry from "@sentry/react";
 
 export default function App() {
-  const [controller, dispatch] = useMaterialUIController();
-  const {
-    miniSidenav,
-    direction,
-    layout,
-    openConfigurator,
-    sidenavColor,
-    transparentSidenav,
-    whiteSidenav,
-    darkMode,
-  } = controller;
-  const [onMouseEnter, setOnMouseEnter] = useState(false);
-  const [rtlCache, setRtlCache] = useState(null);
-  const { pathname } = useLocation();
+  try {
+    // const a = false;
+    // if (!a) {
+    //   throw new Error("This is a test on repo fe");
+    // }
 
-  // Cache for the rtl
-  useMemo(() => {
-    const cacheRtl = createCache({
-      key: "rtl",
-      stylisPlugins: [rtlPlugin],
-    });
+    const [controller, dispatch] = useMaterialUIController();
+    const {
+      miniSidenav,
+      direction,
+      layout,
+      openConfigurator,
+      sidenavColor,
+      transparentSidenav,
+      whiteSidenav,
+      darkMode,
+    } = controller;
+    const [onMouseEnter, setOnMouseEnter] = useState(false);
+    const [rtlCache, setRtlCache] = useState(null);
+    const { pathname } = useLocation();
 
-    setRtlCache(cacheRtl);
-  }, []);
+    // Cache for the rtl
+    useMemo(() => {
+      const cacheRtl = createCache({
+        key: "rtl",
+        stylisPlugins: [rtlPlugin],
+      });
 
-  // Open sidenav when mouse enter on mini sidenav
-  const handleOnMouseEnter = () => {
-    if (miniSidenav && !onMouseEnter) {
-      setMiniSidenav(dispatch, false);
-      setOnMouseEnter(true);
-    }
-  };
+      setRtlCache(cacheRtl);
+    }, []);
 
-  // Close sidenav when mouse leave mini sidenav
-  const handleOnMouseLeave = () => {
-    if (onMouseEnter) {
-      setMiniSidenav(dispatch, true);
-      setOnMouseEnter(false);
-    }
-  };
-
-  // Change the openConfigurator state
-  const handleConfiguratorOpen = () => setOpenConfigurator(dispatch, !openConfigurator);
-
-  // Setting the dir attribute for the body element
-  useEffect(() => {
-    document.body.setAttribute("dir", direction);
-  }, [direction]);
-
-  // Setting page scroll to 0 when changing the route
-  useEffect(() => {
-    document.documentElement.scrollTop = 0;
-    document.scrollingElement.scrollTop = 0;
-  }, [pathname]);
-
-  const getRoutes = (allRoutes) =>
-    allRoutes.map((route) => {
-      if (route.collapse) {
-        return getRoutes(route.collapse);
+    // Open sidenav when mouse enter on mini sidenav
+    const handleOnMouseEnter = () => {
+      if (miniSidenav && !onMouseEnter) {
+        setMiniSidenav(dispatch, false);
+        setOnMouseEnter(true);
       }
+    };
 
-      if (route.route) {
-        return <Route exact path={route.route} element={route.component} key={route.key} />;
+    // Close sidenav when mouse leave mini sidenav
+    const handleOnMouseLeave = () => {
+      if (onMouseEnter) {
+        setMiniSidenav(dispatch, true);
+        setOnMouseEnter(false);
       }
+    };
 
-      return null;
-    });
+    // Change the openConfigurator state
+    const handleConfiguratorOpen = () => setOpenConfigurator(dispatch, !openConfigurator);
 
-  const configsButton = (
-    <MDBox
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      width="3.25rem"
-      height="3.25rem"
-      bgColor="white"
-      shadow="sm"
-      borderRadius="50%"
-      position="fixed"
-      right="2rem"
-      bottom="2rem"
-      zIndex={99}
-      color="dark"
-      sx={{ cursor: "pointer" }}
-      onClick={handleConfiguratorOpen}
-    >
-      <Icon fontSize="small" color="inherit">
-        settings
-      </Icon>
-    </MDBox>
-  );
+    // Setting the dir attribute for the body element
+    useEffect(() => {
+      document.body.setAttribute("dir", direction);
+    }, [direction]);
 
-  return direction === "rtl" ? (
-    <CacheProvider value={rtlCache}>
-      <ThemeProvider theme={darkMode ? themeDarkRTL : themeRTL}>
+    // Setting page scroll to 0 when changing the route
+    useEffect(() => {
+      document.documentElement.scrollTop = 0;
+      document.scrollingElement.scrollTop = 0;
+    }, [pathname]);
+
+    const getRoutes = (allRoutes) =>
+      allRoutes.map((route) => {
+        if (route.collapse) {
+          return getRoutes(route.collapse);
+        }
+
+        if (route.route) {
+          return <Route exact path={route.route} element={route.component} key={route.key} />;
+        }
+
+        return null;
+      });
+
+    const configsButton = (
+      <MDBox
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        width="3.25rem"
+        height="3.25rem"
+        bgColor="white"
+        shadow="sm"
+        borderRadius="50%"
+        position="fixed"
+        right="2rem"
+        bottom="2rem"
+        zIndex={99}
+        color="dark"
+        sx={{ cursor: "pointer" }}
+        onClick={handleConfiguratorOpen}
+      >
+        <Icon fontSize="small" color="inherit">
+          settings
+        </Icon>
+      </MDBox>
+    );
+
+    return direction === "rtl" ? (
+      <CacheProvider value={rtlCache}>
+        <ThemeProvider theme={darkMode ? themeDarkRTL : themeRTL}>
+          <CssBaseline />
+          {layout === "dashboard" && (
+            <>
+              <Sidenav
+                color={sidenavColor}
+                brand={(transparentSidenav && !darkMode) || whiteSidenav ? brandDark : brandWhite}
+                brandName="Trang quản trị"
+                routes={routes}
+                onMouseEnter={handleOnMouseEnter}
+                onMouseLeave={handleOnMouseLeave}
+              />
+              <Configurator />
+              {configsButton}
+            </>
+          )}
+          {layout === "vr" && <Configurator />}
+          <Routes>
+            {getRoutes(routes)}
+            <Route path="*" element={<Navigate to="/admin/dashboard" />} />
+          </Routes>
+        </ThemeProvider>
+      </CacheProvider>
+    ) : (
+      <ThemeProvider theme={darkMode ? themeDark : theme}>
         <CssBaseline />
         {layout === "dashboard" && (
           <>
@@ -170,29 +201,8 @@ export default function App() {
           <Route path="*" element={<Navigate to="/admin/dashboard" />} />
         </Routes>
       </ThemeProvider>
-    </CacheProvider>
-  ) : (
-    <ThemeProvider theme={darkMode ? themeDark : theme}>
-      <CssBaseline />
-      {layout === "dashboard" && (
-        <>
-          <Sidenav
-            color={sidenavColor}
-            brand={(transparentSidenav && !darkMode) || whiteSidenav ? brandDark : brandWhite}
-            brandName="Trang quản trị"
-            routes={routes}
-            onMouseEnter={handleOnMouseEnter}
-            onMouseLeave={handleOnMouseLeave}
-          />
-          <Configurator />
-          {configsButton}
-        </>
-      )}
-      {layout === "vr" && <Configurator />}
-      <Routes>
-        {getRoutes(routes)}
-        <Route path="*" element={<Navigate to="/admin/dashboard" />} />
-      </Routes>
-    </ThemeProvider>
-  );
+    );
+  } catch (err) {
+    Sentry.captureException(err);
+  }
 }
